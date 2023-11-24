bb60cb52c13e861499877de00ef12eecbf04abb4
jererodri14 <jererodri14@gmail.com>
2023-11-23 22:39:41 -0600

    made a folder and a file for data

---
+++ data/questions.js
@@ -0,0 +1,500 @@
+const data = [
+    {
+        categorie: 'Multiplicacion',
+        levels: [
+            {
+                id: 1,
+                questions: [
+                    {   
+                        id: 1,
+                        question: '2 x 2',
+                        answers: [
+                           { 
+                            option: '4',
+                            isCorrect: true,
+                           },{
+                            option: '5',
+                            isCorrect: false,
+                           },{
+                            option: '6',
+                            isCorrect: false,
+                           },{
+                            option: '7',
+                            isCorrect: false,
+                           }
+                        ]
+                    },
+                    {
+                        id: 2,
+                        question: '2 x 3',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 3,
+                        question: '2 x 4',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '8',
+                                isCorrect: true,
+                            }
+                        ]
+                    },
+                    {
+                        id: 4,
+                        question: '2 x 5',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '10',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 5,
+                        question: '2 x 6',
+                        answers:  [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '12',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 6,
+                        question: '2 x 7',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '14',
+                                isCorrect: true,
+                            }
+                        ]
+                    },
+                    {
+                        id: 7,
+                        question: '2 x 8',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '16',
+                                isCorrect: true,
+                            }
+                        ]
+                    },
+                    {
+                        id: 8,
+                        question: '2 x 9',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '18',
+                                isCorrect: true,
+                            }
+                        ]
+                    },
+                    {
+                        id: 9,
+                        question: '2 x 10',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '20',
+                                isCorrect: true,
+                            }
+                        ]
+                    },
+                    {
+                        id: 10,
+                        question: '2 x 11',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '22',
+                                isCorrect: true,
+                            }
+                        ]
+                    },
+                    {
+                        id: 11,
+                        question: '2 x 12',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '24',
+                                isCorrect: true,
+                            }
+                        ]
+                    },
+                ]
+            },
+            {
+                id: 2,
+                questions: [
+                    {
+                        id: 12,
+                        question: '3 x 2',
+                        answers: [
+                            {  
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 13,
+                        question: '3 x 3',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '9',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 14,
+                        question: '3 x 4',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '12',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 15,
+                        question: '3 x 5',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '15',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 16,
+                        question: '3 x 6',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '18',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 17,
+                        question: '3 x 7',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '21',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 18,
+                        question: '3 x 8',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '24',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 19,
+                        question: '3 x 9',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '27',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 20,
+                        question: '3 x 10',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '5',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '30',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 21,
+                        question: '3 x 11',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '33',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                    {
+                        id: 22,
+                        question: '3 x 12',
+                        answers: [
+                            {
+                                option: '4',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '36',
+                                isCorrect: true,
+                            },
+                            {
+                                option: '6',
+                                isCorrect: false,
+                            },
+                            {
+                                option: '7',
+                                isCorrect: false,
+                            }
+                        ]
+                    },
+                ]
+            },
+        ]
+    }
+];
+
+export default data;
